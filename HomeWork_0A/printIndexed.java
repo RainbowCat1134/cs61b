3b2f7e11753fd19f695b3bf63fbeddc737219845
RainbowCat1134 <1486042714@qq.com>
2026-03-19 23:19:12 +0800

    add printIndexed.java

---
+++ HomeWork_0A/printIndexed.java
@@ -0,0 +1,16 @@
+public class printIndexed {
+
+    public static void printIndexed(String s){
+        String output = "";
+        for(int i = 0;i < s.length();++i){
+            output += s.charAt(i);
+            output += (s.length() - 1 - i);
+        }
+        System.out.println(output);
+    }
+
+    public static void main(String[] args){
+        String s = "I love apples";
+        printIndexed(s);
+    }
+}
